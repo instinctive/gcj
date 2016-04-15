64ac7f81cd5c0f4d33ab3b7fc9df0b59f05677a7
Charles Martin <charles.martin@gmail.com>
2016-04-15 13:41:10 -0700

    drastic simplification prior to round 1a

--- 2016/utils/README.md
+++ 2016/utils/README.md
@@ -3,19 +3,4 @@ GCJ
 
 Definitions for competing in the Google Code Jam.
 
-Example structure for a problem solution:
-
-    module Main where
-    import GCJ
-    main = jam soln Single
-    soln = getList >>= out . solve where
-        out = putLine . show
-    solve [x,y] = x + y
-
-The `jam` function reads the number of test cases, prints the `Case #X:` statements, and correctly outputs the `Single` or `Multi` line output.
-
-The `soln` function reads the input and writes the output for each test case.
-
-The `solve` function is a purely functional solution to the problem.
-
 Example: [Google Code Jam 2016 Qualification Round](https://github.com/instinctive/gcj2016q)
--- 2016/utils/src/GCJ.hs
+++ 2016/utils/src/GCJ.hs
@@ -1,97 +1,28 @@
 {-| Definitions for competing in the Google Code Jam.
-
-    Example structure for a problem solution:
-
-    @
-    module Main where
-    import GCJ
-    main = jam soln Single
-    soln = getList >>= out . solve where
-        out = putLine . show
-    solve [x,y] = x + y
-    @
-
-    The 'jam' function reads the number of test cases,
-    prints the @Case #X:@ text, and correctly outputs
-    the 'Single' or 'Multi' line output.
-
-    The 'soln' function reads the input and writes the output for each
-    test case.
-
-    The 'solve' function is a purely functional solution to the problem.
 -}
 
 module GCJ (
-    -- * Types
-      Soln, Out(..)
-    -- ** Internal Types
-    , Jam, S
     -- * Solve a codejam problem.
-    , jam, jamCase, jamFile
-    -- * Input and Output
-    , getOne, getList, getString
-    , putLine
+      single, multi
+    -- * Useful input functions.
+    , getOne, getList
     ) where
 
 import Control.Monad (forM_)
-import System.IO (Handle, IOMode(..), stdin, hGetLine, withFile)
-import Control.Monad.Trans.State.Strict (StateT, evalStateT, get)
-import Control.Monad.IO.Class (liftIO)
 
--- | The internal state: input file handle and output string producer.
-type S = Handle
+-- | Read a single value from a stdin line.
+getOne :: Read a => IO a
+getOne = fmap read getLine
 
--- | The internal state monad.
-type Jam a = StateT S IO a
+-- | Read a list of values from a stdin line.
+getList :: Read a => IO [a]
+getList = fmap (map read . words) getLine
 
--- | The solution for a single test case.
-type Soln = Jam ()
+-- | Read the number of test cases and run each case with single-line output.
+single = run ": "
 
--- | The output style for this problem.
-data Out
-    = Single    -- ^ The output is on the same line as the @Case #X:@ statement.
-    | Multi     -- ^ The output is on multiple lines following the @Case #X:@ statement.
-    deriving Eq
+-- | Read the number of test cases and run each case with multi-line output.
+multi  = run ":\n"
 
-mapLine :: (String -> a) -> Jam a
-mapLine f = get >>= liftIO . fmap f . hGetLine
-
--- | Read a single value from an input line.
-getOne :: Read a => Jam a
-getOne = mapLine read
-
--- | Read a list of values from an input line.
-getList :: Read a => Jam [a]
-getList = mapLine (map read . words)
-
--- | Return the input line as a string (without a final '\n').
-getString :: Jam String
-getString = mapLine id
-
-putString :: String -> Jam ()
-putString = liftIO . putStr
-
--- | Output the string and a '\n'.
-putLine :: String -> Jam ()
-putLine = liftIO . putStrLn
-
-jamHandle :: Soln -> Out -> Handle -> IO ()
-jamHandle m o h = flip evalStateT h $ do
-    n <- getOne
-    forM_ [1..n] $ \i -> putString (casestr i) >> m
-  where
-    casestr i = "Case #" ++ show i ++ final o
-    final Single = ": "
-    final Multi  = ":\n"
-
--- | Read a problem from a file, solution to stdout.
-jamFile :: Soln -> Out -> FilePath -> IO ()
-jamFile m o p = withFile p ReadMode $ jamHandle m o
-
--- | Read a problem from stdin, solution to stdout.
-jam :: Soln -> Out -> IO ()
-jam m o = jamHandle m o stdin
-
--- | Read a case from stdin, solution to stdout.
-jamCase :: Soln -> Out -> IO ()
-jamCase m o = flip evalStateT stdin m
+run sep m = getOne >>= mapM_ m' . enumFromTo 1 where
+    m' i = (putStr $ "Case #" ++ show i ++ sep) >> m
