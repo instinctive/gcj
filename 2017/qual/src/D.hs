5be7fa18369af0e9e02c7e780bf52983a7b19c07
Charles Martin <charles.martin@gmail.com>
2017-04-09 22:46:30 -0700

    Cleanup

--- 2017/qual/src/D.hs
+++ 2017/qual/src/D.hs
@@ -16,12 +16,12 @@ import Data.Tuple
 import System.IO (stderr, hPutChar, hFlush)
 import qualified Debug.Trace as DBG
 -- }}}
--- my imports {{{
+
 import Control.Lens
 import Data.List ((\\))
 import Linear.V2
-import qualified Data.IntSet        as S
--- }}}
+import qualified Data.IntSet as S
+
 -- gcj {{{
 gcj :: IO () -> IO ()
 gcj docase = do
@@ -40,14 +40,12 @@ getReadList :: Read a => IO [a]
 getReadList = fmap read . words <$> getLine
 -- }}}
 
--- types {{{
 type Z = Int
 type X = V2 Int
 type Q = Char
 type Model = (X,Q)
--- }}}
 
-main :: IO () -- {{{
+main :: IO ()
 main = gcj $ do
     [n,m] <- getReadList
     mm <- replicateM m $ mk . words <$> getLine
@@ -57,13 +55,12 @@ main = gcj $ do
   where
     mk [s,r,c] = (V2 (read r) (read c), head s)
     out (V2 r c, q) = putStrLn . unwords $ [[q], show r, show c] 
--- }}}
 
-small :: Z -> [Model] -> (Z, [Model]) -- {{{
+small :: Z -> [Model] -> (Z, [Model])
 small 1 [(_,'o')] = (2,[])
 small 1 _ = (2,[(V2 1 1,'o')])
 small n mm = (n*3-2, row1 <> diags <> rown) where
-    gaps = f <$> filter (not.(`S.member` cset)) [1..n] where f c = mk c '+'
+    gaps = (`mk` '+') <$> filter (not.(`S.member` cset)) [1..n]
     cset = S.fromList $ mm ^..traverse._1._y
     (ocol, row1) = case find ((/='+').snd) mm of
         Just (V2 1 c,'o') -> (c, gaps)
@@ -75,4 +72,3 @@ small n mm = (n*3-2, row1 <> diags <> rown) where
         cc = if ocol < n then [1..n] else [n,n-1..1]
     rown = (,'+') <$> V2 n <$> [2..n-1] 
     mk c q = (V2 1 c, q)
--- }}}
